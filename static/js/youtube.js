7667e7d0976b697cee8500b367b487b6a578a418
David Kebler <d@kebler.net>
2017-04-11 20:08:03 -0700

    add in settings to turn off youtube thumb images on demand globally and per shortcode

--- static/js/youtube.js
+++ static/js/youtube.js
@@ -2,27 +2,41 @@
 
   $(document).bind("DOMContentLoaded",
     function () {
-      // TODO also grad maxwith and window padding data
+
       var playBtn = '<i class="fa fa-play-circle-o play-button"></i>'
       $('div[youtube_id]').each(function () {
-        var vid = $(this).attr('youtube_id');
-        var mw = $(this).attr('maxWidth');
-        var wp = $(this).attr('wPad');
-        var bgi = `style="background-image: url(https://i.ytimg.com/vi/${vid}/mqdefault.jpg)"`
-        var thumb = `<div class="thumb--yt" ${bgi} width="560" height="315">`
-        $(this).append(thumb).children().append(playBtn).click(insertIframe).fitToWindow(mw, wp)
+        if ($(this).attr('nothumb') !== "yes") {
+          var vid = $(this).attr('youtube_id');
+          var mw = $(this).attr('maxWidth');
+          var wp = $(this).attr('wPad');
+          var bgi = `style="background-image: url(https://i.ytimg.com/vi/${vid}/mqdefault.jpg)"`
+          var thumb = `<div class="thumb--yt" ${bgi} width="560" height="315">`
+          $(this).append(thumb).children().append(playBtn).click(insertIframe).fitToWindow(mw, wp)
+
+        } else { // if image thumbs turned off
+          insertIframe.call(this)
+        }
+
       })
-    });
+    })
 
   function insertIframe() {
     var $box = $(this).parent()
     var id = $box.attr('youtube_id')
+    var autoplay = "autoplay=1&"
+    if (!id) { // image thumb is off not called by thumb so don't need parent
+      $box = $(this)
+      id = $box.attr('youtube_id')
+      autoplay = ""
+    }
+    var mw = $box.attr('maxWidth');
+    var wp = $box.attr('wPad');
     var start = $box.attr("start") || 1
     var list = ""
     if ($box.attr("list")) { list = `&list=${$box.attr("list")}` }
-    var url = `https://www.youtube.com/embed/${id}?autoplay=1&start=${start}`
+    var url = `https://www.youtube.com/embed/${id}?${autoplay}start=${start}`
     var vIframe = `<iframe width = "560" height = "315" src ="${url}${list}" frameborder = "0" allowfullscreen></iframe>`
-    $box.html(vIframe).children().fitToWindow($box.data("maxWidth"), $box.data("wPad"))
+    $box.html(vIframe).children().fitToWindow($box.data("maxWidth") || mw, $box.data("wPad") || wp)
   }
 
 }(jQuery));
